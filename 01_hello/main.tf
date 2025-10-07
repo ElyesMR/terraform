8dbb953988905a390f1e62c102e7b6770a6fe10e
Elyes <vitaelyes92izii@gmail.com>
2025-10-07 19:03:37 +0200

    Exercice 1.1  Hello Terraform

---
+++ 01_hello/main.tf
@@ -0,0 +1,10 @@
+﻿terraform {
+  required_version = ">= 1.3.0"
+}
+
+# Aucune ressource externe : juste un echo local
+resource "null_resource" "hello" {
+  provisioner "local-exec" {
+    command = "echo Hello Terraform!"
+  }
+}
