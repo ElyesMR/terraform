cbf049eee99cfba627f436dcac671a44740b9b4c
ElyesMR <148759427+ElyesMR@users.noreply.github.com>
2025-10-08 09:49:05 +0200

    main.tf

--- 01_hello/main.tf
+++ 01_hello/main.tf
@@ -2,7 +2,7 @@
   required_version = ">= 1.3.0"
 }
 
-# Aucune ressource externe : juste un echo local
+
 resource "null_resource" "hello" {
   provisioner "local-exec" {
     command = "echo Hello Terraform!"
